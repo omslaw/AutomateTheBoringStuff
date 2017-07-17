57543ace6bca827e03e7262b0b7ca38067a02c2f
omslaw <cafeconleche@overhere.com>
2017-07-16 23:00:58 -0700

    end of day upload
    done for the day

--- mapIt.py
+++ mapIt.py
@@ -3,12 +3,15 @@
 # command line or clipboard
 
 import webbrowser, sys
-
+import pyperclip
 
 
 if len(sys.argv) > 1:
     # get address from command line
     address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard.
+    address = pyperclip.paste()
 
 webbrowser.open('https://www.google.com/maps/place/' + address)
 print ('yo')
---
+++ request.py
@@ -0,0 +1,5 @@
+import requests
+
+res = requests.get('http://automatetheboringstuff.com/files/rj.txt')
+type (res)
+
